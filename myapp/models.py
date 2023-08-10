95855233c697572d99810d32fe2549c03322883f
james ignacio olaya garzon <jamesmixolaya@gmail.com>
2023-08-10 01:44:03 -0500

    verificacion de commit

--- myapp/models.py
+++ myapp/models.py
@@ -12,3 +12,4 @@ class Task(models.Model):
     title = models.CharField(max_length=200, help_text='ingrese el título de la tarea.')
     descrption = models.TextField(help_text='ingrese la descripción de la tarea')
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    
