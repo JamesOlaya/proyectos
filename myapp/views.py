0cb232e61cf7c7f31fca9e81d3f1208ead61e967
james ignacio olaya garzon <jamesmixolaya@gmail.com>
2023-08-16 06:14:15 -0500

    actualizacion del 16 de agosto

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-from .forms import CreateNewTask,CreatenewProject
+from .forms import CreateNewTask,CreateNewProject
+
+
 # Create your views here.
 
 def index(request):
@@ -31,7 +33,7 @@ def projects(request):
     #project = get_object_or_404(list(Project.objects.values()), name= p_name )
     #return JsonResponse(project.name, safe=False)
     title = 'Welcome to Django Projects'
-    return render(request, 'projects.html',{
+    return render(request, 'Projects/projects.html',{
         'title': title,
         'projects': projects
     })
@@ -41,29 +43,43 @@ def tasks(request):
     #task = get_object_or_404(Task, id=id)
     #return render('<h1>tareas %s</h1>' % task.title)
     tasks = Task.objects.all()
-    tasks2 =list(Task.objects.values())
-    return render(request, 'tasks.html',{
+    return render(request, 'Tasks/tasks.html',{
         'tasks': tasks, 
     })
 # hacer las modificaciones necesarias para que busque un proyecto por el nombre
 
 def create_task(request):
     if request.method == 'GET':
-        return render(request, 'create_task.html',{
+        return render(request, 'Tasks/create_task.html',{
             'form': CreateNewTask()
         })
     else:
         title = request.POST['title']
         descrption = request.POST['description']
-        project_id = 2
+        project_id = request.POST['project_id']
         Task.objects.create(title =title, descrption=descrption, project_id=project_id)
         return redirect('/tasks')
     
 def create_project(request):
     if request.method == 'GET':
-        return render(request,'create_project.html', {
-            'form': CreatenewProject()
+        return render(request,'Projects/create_project.html', {
+            'form': CreateNewProject()
         })
     else:
         name = request.POST['name']
-        pro
+        Project.objects.create(name = name)
+        return redirect('/projects')
+def tasks_project(request, project_id):
+    project = Project.objects.get(id=project_id)
+    tasks = Task.objects.all().filter(project_id=project_id)
+    return render(request, 'Tasks/projects.thml',{
+        'project': project.name,
+        'tasks': tasks
+    })
+
+
+# gestionar los datos de las bibliotecsa de una universidad, de cada biblioteca interasa el nombre de la biblioteca y su ubicacion en la universidad, cada biblioteca tiene una coleccion de libros en donde para cada libro interesa saber el nombre, el isbn, autor y el estado, que ele stado puede ser en sala o prestado, interesa poder agregar nuevos libros para cada colecion de cada biblioteca.
+
+# el modelo de la abse de datos
+# las historias de usuario segun el enunciado
+#mañana se codifica todo
